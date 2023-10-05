ec0825aaa75fcbc5835acb899f86cd6e941eda97
Mao De Matos <mao.dematos@gmail.com>
2023-10-05 20:00:38 +0200

    ✨Add basic working example with TailwindCSS

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,28 @@
 use leptos::*;
 
 fn main() {
-    mount_to_body(|| view! { <p>"Hello world!"</p> })
+    // _ = console_log::init_with_level(log::Level::Debug);
+    // console_error_panic_hook::set_once();
+
+    logging::log!("CSR mode - mounting to body.");
+
+    mount_to_body(App)
 }
 
+#[component]
+fn App() -> impl IntoView {
+    let (count, set_count) = create_signal(0);
+
+    view! {
+        <div class="p-5 flex flex-col justify-center items-center gap-4">
+            <p>
+                {move || count.get()}
+            </p>
+            <button class="px-3 py-2 bg-rose-800 rounded-md transition-colors hover:bg-rose-700" on:click=move |_| {
+                set_count.update(|n| *n += 1);
+            }>
+                "Click me!"
+            </button>
+        </div>
+    }
+}
